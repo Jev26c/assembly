993253b164f6e9f8405d6389dea099d738d2e817
Jev <45295357+J-cham@users.noreply.github.com>
2019-09-06 17:24:11 +0200

    Update Dillbert.s

--- Dillbert.s
+++ Dillbert.s
@@ -1,1 +1,17 @@
+.data
 
+.global main
+
+main:
+
+	movq $0, %rdi
+	call exit
+
+encodeRlE:
+	push %rbp		#store old basepointer on the stack
+	movq %rsp, %rbp 	#make the stackpointer
+
+	movq %rsp, %rbp		#restore the basepointer
+	popq %rbp               #pop %rbp of the stack
+	
+	
